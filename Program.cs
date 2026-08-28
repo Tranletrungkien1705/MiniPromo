b7f7b4ef51d8b2c4d1f0bdea1782dea25c3c1ea8
toannh <toannh@idocnet.com>
2026-08-28 18:44:46 +0700

    Add fleet observability (Serilog+OpenSearch correlation-id, Redis, Swagger)

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using MiniPromo.Data;
 using MiniPromo.Models;
 using MiniPromo.Services;
+using Serilog;
 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+FleetObs.ConfigureLogger("minipromo");
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
 builder.WebHost.UseUrls($"http://0.0.0.0:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}");
 
 var conn = Environment.GetEnvironmentVariable("CONNECTION_STRING")
@@ -17,11 +20,14 @@ builder.Services.AddDbContext<AppDbContext>(o =>
 });
 builder.Services.AddScoped<ITenantContext, TenantContext>();
 builder.Services.AddScoped<IPromoService, PromoService>();
+builder.Services.AddFleetObs();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
     await Seeder.SeedAsync(scope.ServiceProvider.GetRequiredService<AppDbContext>());
+
+app.UseFleetObs();
 
 app.Use(async (ctx, next) =>
 {
